ee759f38006a3e8d7885d3c490d386eb17b9621c
Karol Majta <karolmajta@gmail.com>
2013-09-11 22:18:13 +0200

    Added better serializer and filtering to cityknowledges

--- apps/towngeek/locations/api/cityknowledges.py
+++ apps/towngeek/locations/api/cityknowledges.py
@@ -2,32 +2,43 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import permissions
 
-from filthy.views import WrappedResultMixin
+from filthy.views import WrappedResultMixin, FilterMixin
 
 from towngeek.commons.permissions import IsObjectOwner
 from towngeek.locations.models import CityKnowledge
-from towngeek.locations.serializers import CityKnowledgeSerializer
+from towngeek.locations.serializers.cityknowledges import \
+    CityKnowledgeSerializer, WritableCityKnowledgeSerializer
 
 
 class CityKnowledgeDetailView(WrappedResultMixin, RetrieveDestroyAPIView):
 
-    queryset = CityKnowledge.objects.all()
+    queryset = CityKnowledge.objects.select_related('user', 'city')
     serializer_class = CityKnowledgeSerializer
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsObjectOwner('user', allow_read=True),)
 
 
-class CityKnowledgeListCreateView(ListCreateAPIView):
+class CityKnowledgeListCreateView(FilterMixin, ListCreateAPIView):
 
-    queryset = CityKnowledge.objects.all()
-    serializer_class = CityKnowledgeSerializer
+    queryset = CityKnowledge.objects.select_related('user', 'city')
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
-    paginate_by = 10
+    paginate_by = 100
+
+    filters = {
+        'user': ('user__id', lambda val: int(val)),
+    }
 
     def pre_save(self, obj):
         obj.user = self.request.user
+
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return CityKnowledgeSerializer
+        else:
+            return WritableCityKnowledgeSerializer
--- apps/towngeek/locations/serializers/cityknowledges.py
+++ apps/towngeek/locations/serializers/cityknowledges.py
@@ -2,6 +2,8 @@
 from rest_framework import serializers
 
 from towngeek.locations.models import CityKnowledge
+from towngeek.locations.serializers.cities import CitySerializer
+from towngeek.commons.serializers.users import SafeUserSerializer
 
 
 class CityKnowledgeSerializer(serializers.ModelSerializer):
@@ -9,4 +11,16 @@ class CityKnowledgeSerializer(serializers.ModelSerializer):
     class Meta:
         model = CityKnowledge
         fields = ('id', 'user', 'city')
-        read_only_fields = ('id', 'user')
+        read_only_fields = ('id',)
+
+    user = SafeUserSerializer()
+    city = CitySerializer()
+
+
+class WritableCityKnowledgeSerializer(CityKnowledgeSerializer):
+
+    class Meta(CityKnowledgeSerializer.Meta):
+        pass
+
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    city = serializers.PrimaryKeyRelatedField()
